c26b71b561d240afddc17420de79296ab21cead1
jordantangy@gmail.com <jordantangy@gmail.com>
2020-06-11 14:18:11 +0300

    implemention of FootCommander

--- FootCommander.cpp
+++ FootCommander.cpp
@@ -2,9 +2,42 @@
 
 
  std::pair<int,int> FootCommander::activity(std::vector<std::vector<Soldier*>> board ,std::pair<int,int> location){
-     return std::pair<int,int>{-1,-1};
+    std::pair<int,int> target = search(board, location, this->get_player());
+    if (target.first != -1){
+        board[target.first][target.second]->injured(this->_DP);
+    }
+    for (size_t i = 0; i < board.size(); i++){
+        for (size_t j = 0; j < board.size(); j++){
+            Soldier *s=board[i][j];
+            if(s->_player ==  this->_player){
+                s->activity(board,location);
+            }
+        }
+    }
+    
+    return target;
 }
 
+std::pair<int,int> FootCommander::search(std::vector<std::vector<Soldier*>> board, std::pair<int,int> location, int player_number){
+    std::pair<int,int> ans {-1,-1};
+    int min = -1;
+    int temp;
+    for (int i = 0; i < board.size(); i++) {
+        for (int j = 0; j < board[i].size(); j++) {
+            if (board[i][j] != nullptr){
+                if (board[i][j]->get_player() != player_number) {
+                    temp = (abs(location.first - i)) + (abs(location.second - j));
+                    if (temp < min || min == -1){
+                        min = temp;
+                        ans = {i,j};
+                    }
+                }
+            }
+        }
+            
+    }
+    return ans;
+}
 FootCommander::~FootCommander(){
     
 }
--- FootCommander.hpp
+++ FootCommander.hpp
@@ -7,5 +7,6 @@ class FootCommander : public Soldier{
 public:
     FootCommander(int player) : Soldier(150, 20, player){}
     std::pair<int,int> activity(std::vector<std::vector<Soldier*>> board ,std::pair<int,int> location);
+    std::pair<int,int> FootSoldier::search(std::vector<std::vector<Soldier*>> board, std::pair<int,int> location, int player_number);
     ~FootCommander();
 };
